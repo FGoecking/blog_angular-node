e8df6f7444aaa529210adbd4a2bf15dc4d93fb90
FGoecking <flaviogoecking@gmail.com>
2021-09-28 22:03:49 -0300

    create sidenav

--- front-end/blog-app/src/app/app.module.ts
+++ front-end/blog-app/src/app/app.module.ts
@@ -7,12 +7,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatIconModule } from '@angular/material/icon'
-import { MatButtonModule } from '@angular/material/button'
+import { MatButtonModule } from '@angular/material/button';
+import { NavegationComponent } from './components/template/navegation/navegation.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    NavegationComponent,
   ],
   imports: [
     BrowserModule,
@@ -20,7 +24,9 @@ import { MatButtonModule } from '@angular/material/button'
     BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
